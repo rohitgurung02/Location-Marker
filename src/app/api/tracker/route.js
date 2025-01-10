9a7bfb8ff012d6b71c61237f8f5999f2f2ad6305
unknown <rohitgurung086@gmail.com>
2025-01-10 13:51:16 +0530

    mongoDB cloud has been connneted

--- src/app/api/tracker/route.js
+++ src/app/api/tracker/route.js
@@ -1,39 +1,33 @@
-import { NextResponse } from "next/server";
-import { ConnectDB } from "../../../../lib/config/db";
-import locationTracker from "../../../../lib/config/models/locationTracker";
+import { NextResponse } from 'next/server';
+import mongoDB from '../../../../lib/mongoDB/mongoDB'
+import locationTracker from '../../../../lib/config/models/locationTracker';
 
 export async function GET(request) {
-  console.log('location added')
-  return NextResponse.json({msg:'API Working'})
+  console.log('location added');
+  return NextResponse.json({ msg: 'API Working' });
 }
 
-// through this function it's connect to the DataBase(Mongo DB);
-const LoadDB = async () => {
-  await ConnectDB();
-};
-
-LoadDB();
-
-// In this method we can store the data into the DataBase.
 export async function POST(request) {
-  // We get the data as a (Form Data)
+  // Get the data as Form Data
   const formData = await request.formData();
 
-  // We can get the (Date and Current Time)
+  // Get the current timestamp
   const timestamp = Date.now();
 
+  // Prepare location data
   const location = {
-    longitude: `${formData.get("longitude")}`,
-    latitude: `${formData.get("latitude")}`,
-    potholes: `${formData.get("potholes")}`,
-    animalProneAreas: `${formData.get("animalProneAreas")}`,
+    longitude: `${formData.get('longitude')}`,
+    latitude: `${formData.get('latitude')}`,
+    potholes: `${formData.get('potholes')}`,
+    animalProneAreas: `${formData.get('animalProneAreas')}`,
   };
+
+  // Connect to MongoDB
+  await mongoDB();
 
   // Save to MongoDB
   await locationTracker.create(location);
-  console.log("Location Saved");
+  console.log('Location Saved');
 
-  return NextResponse.json({ success: true, msg: "All Data Added" });
+  return NextResponse.json({ success: true, msg: 'All Data Added' });
 }
-
-
