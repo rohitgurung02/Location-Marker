3b31e5657b4b0acdc072b70ae8a3a6d3b4bfbcba
unknown <rohitgurung086@gmail.com>
2025-01-11 16:38:29 +0530

    html form updated with route

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,15 @@
 "use client";
 
 import { useState, useEffect } from "react";
-
-import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+import dynamic from "next/dynamic";
 import "leaflet/dist/leaflet.css";
+
+// Dynamically import react-leaflet components to avoid SSR issues
+const MapContainer = dynamic(() => import("react-leaflet").then((mod) => mod.MapContainer), { ssr: false });
+const TileLayer = dynamic(() => import("react-leaflet").then((mod) => mod.TileLayer), { ssr: false });
+const Marker = dynamic(() => import("react-leaflet").then((mod) => mod.Marker), { ssr: false });
+const Popup = dynamic(() => import("react-leaflet").then((mod) => mod.Popup), { ssr: false });
+
 export default function Home() {
   const [userPosition, setUserPosition] = useState(null);
   const [locations, setLocations] = useState([]);
@@ -16,7 +22,6 @@ export default function Home() {
         const data = await response.json();
 
         if (data.success) {
-        
           const validLocations = data.data.map((loc) => ({
             locationLatitude: parseFloat(loc.latitude),
             locationLongitude: parseFloat(loc.longitude),
@@ -86,19 +91,19 @@ export default function Home() {
 
   return (
     <div>
-      <MapContainer center={userPosition} zoom={15} style={{ height: "100vh", width: "100%" }}>
-      <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
-      {locations.map((loc, index) => (
-        <Marker key={index} position={[loc.locationLatitude, loc.locationLongitude]}>
-          <Popup>{loc.locationName}</Popup>
-        </Marker>
-      ))}
-      {userPosition && (
-        <Marker position={userPosition}>
-          <Popup>You are here</Popup>
-        </Marker>
-      )}
-    </MapContainer>
+      <MapContainer center={userPosition || [0, 0]} zoom={15} style={{ height: "100vh", width: "100%" }}>
+        <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+        {locations.map((loc, index) => (
+          <Marker key={index} position={[loc.locationLatitude, loc.locationLongitude]}>
+            <Popup>{loc.locationName}</Popup>
+          </Marker>
+        ))}
+        {userPosition && (
+          <Marker position={userPosition}>
+            <Popup>You are here</Popup>
+          </Marker>
+        )}
+      </MapContainer>
     </div>
   );
 }
